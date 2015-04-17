e7ea9683c974974b2c26e94b7897f542865a7acd
Kieran Barry <kieran_barry@brown.edu>
2015-04-17 03:26:51 -0400

    login works

--- src/main/java/edu/brown/cs/signMeUpBeta/handlers/QueueHandler.java
+++ src/main/java/edu/brown/cs/signMeUpBeta/handlers/QueueHandler.java
@@ -152,7 +152,7 @@ public class QueueHandler {
     @Override
     public ModelAndView handle(final Request req, final Response res) {
       String courseAndUserId = req.params(":courseAndUserId");
-      String[] reqParams = courseAndUserId.split("?");
+      String[] reqParams = courseAndUserId.split("~");
       System.out.println(courseAndUserId);
       String courseId = reqParams[0];
       String login = reqParams[1];
@@ -164,7 +164,7 @@ public class QueueHandler {
       String subQuestions = ""; //JSON array?
       Map<String, Object> variables =
           new ImmutableMap.Builder().put("title", "SignMeUp 2.0")
-          .put("courseId", courseId).put("login", login)
+          .put("course", courseId).put("login", login)
           .put("assignment", assignment).put("questions", questions)
           .put("subQuestions", subQuestions).build();
       return new ModelAndView(variables, "signUpForHours.html");
