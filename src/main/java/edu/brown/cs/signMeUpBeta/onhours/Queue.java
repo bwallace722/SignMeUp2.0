e5e4c6a035d13ce078958e6744582875d84bbf23
Kieran Barry <kieran_barry@brown.edu>
2015-04-17 16:15:19 -0400

    queue 'implemented'

--- src/main/java/edu/brown/cs/signMeUpBeta/onhours/Queue.java
+++ src/main/java/edu/brown/cs/signMeUpBeta/onhours/Queue.java
@@ -1,19 +1,54 @@
 package edu.brown.cs.signMeUpBeta.onhours;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 import edu.brown.cs.signMeUpBeta.student.Account;
 
 public class Queue {
-  public Queue() {}
   
-  public void add(Account s) {}
+  private PriorityQueue<Account> pq;
+  private double cutOff;
   
-  public void remove(Account s) {}
-  
-  public void switchOrder(Account s1, Account s2) {}
-  
-  public double getPriority(Account s1) {
-    return 0;
+  public Queue() {
+    pq = new PriorityQueue(new PriorityComp());
+    cutOff = Double.POSITIVE_INFINITY;
   }
   
-  public void setCuttOff(double time) {}
+  private class PriorityComp implements Comparator<Account> {
+
+    public PriorityComp() {}
+
+    @Override
+    public int compare(Account a, Account b) {
+      double aPrior = a.priority();
+      double bPrior = b.priority();
+
+      if (aPrior > bPrior) {
+        return 1;
+      } else if (aPrior < bPrior) {
+        return -1;
+      } else {
+        return 0;
+      }
+    }
+  }
+  
+  public void add(Account s) {
+    pq.add(s);
+  }
+  
+  public void remove(Account s) {
+    pq.remove(s);
+  }
+  
+//  public void switchOrder(Account s1, Account s2) {}
+  
+  public double getPriority(Account s1) {
+    return s1.priority();
+  }
+  
+  public void setCutOff(double time) {
+    cutOff = time;
+  }
 }
